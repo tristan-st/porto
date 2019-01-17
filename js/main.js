b6fbc01b37c2e61a6abeee33ab8813d61d9ba949
tristan-st <merlijnstinkt@hotmail.com>
2019-01-17 11:13:51 +0100

    hoi

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".project1").on("mouseenter", function () {
+    $(".overlay1").css('opacity', '1');
+}).on("mouseleave", function () {
+    $(".overlay1").css('opacity', '0');
+});
+
+$(".project2").on("mouseenter", function () {
+    $(".overlay2").css('opacity', '1');
+}).on("mouseleave", function () {
+    $(".overlay2").css('opacity', '0');
+});
+
+$(".project3").on("mouseenter", function () {
+    $(".overlay3").css('opacity', '1');
+}).on("mouseleave", function () {
+    $(".overlay3").css('opacity', '0');
+});
---
+++ projecten/scouting/js/javascript.js
@@ -0,0 +1,43 @@
+var currentStep = 1;
+var isSwitching = 0;
+
+$('#next-step').click(function() {
+  var counter = $('#js-progressbar').val();
+  counter += 11.1111111111;
+  console.log(counter);
+  $('#js-progressbar').val(counter);
+  if (currentStep < 20 && isSwitching == 0) {
+    isSwitching++;
+    if (currentStep == 9) {
+      // Result stage
+      $('#prev_next').css('display', 'none');
+      $('#change_submit').css('display', 'inline-block');
+    }
+    $('#tab-' + currentStep).fadeOut(200, function() {
+
+      currentStep++;
+      $('#tab-' + currentStep).fadeIn(200, function() {
+        isSwitching--;
+      });
+
+    });
+  }
+});
+
+$('#previous-step').click(function() {
+  var counter = $('#js-progressbar').val();
+  counter-=10;
+  console.log(counter);
+  $('#js-progressbar').val(counter);
+  if (currentStep > 1 && isSwitching == 0) {
+    isSwitching++;
+    $('#tab-' + currentStep).fadeOut(200, function() {
+
+      currentStep--;
+      $('#tab-' + currentStep).fadeIn(200, function() {
+        isSwitching--;
+      });
+
+    });
+  }
+});
